e1057b992137c2232ba200e8280f28f357801ee1
rihab <rehabalyasiri@yahoo.com>
2019-10-22 22:02:31 +0200

    Calculator

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 var result = document.querySelector('.result');
+var plus = document.querySelector('.plus');
 
 
 var myCollection = document.getElementsByTagName('input');
@@ -8,16 +9,24 @@ var txt = " ";
 for(var i = 0; i< myCollection.length ; i++) {
     myCollection[i].addEventListener('click',function(e) {
         if(e.target.defaultValue=="="){
-            txt=txt
+            txt=txt;
+            console.log("after = " +txt)
         } else if(e.target.defaultValue=="C"){
             txt="";
             result.innerHTML = txt;
+            console.log("after c " +txt)
         } else if(e.target.defaultValue=="Del"){
-            txt="";
+            txt = txt.substring(0,txt.length-1);
+             result.innerHTML = txt;
+             console.log("after del " +txt)
+        } else if(txt[1] == '+') {
+            txt= " "
             result.innerHTML = txt;
-        } else{
+            console.log("after + " +txt)
+        }else{
           txt += e.target.defaultValue;
           result.innerHTML = txt;  
+          console.log(" " +txt)
         }
     });
 }
@@ -25,8 +34,14 @@ for(var i = 0; i< myCollection.length ; i++) {
 
 document.getElementById("equal").addEventListener("click",()=>{
     // console.log(eval(document.getElementById("result").innerHTML));
-    result.innerHTML = eval(document.getElementById("result").innerHTML); 
+    // result.innerHTML = eval(document.getElementById("result").innerHTML); 
+    txt = eval(document.getElementById("result").innerHTML);
+    result.innerHTML = txt;
 })
+
+// var str = "helloworld";
+// var last = str.substring(0,1);
+// console.log(last); 
 
 
 
