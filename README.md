48007c63ed386c93651744d5798d9ce502e2be31
rihab <rehabalyasiri@yahoo.com>
2019-10-26 12:12:27 +0200

    updating the calucaltor

---
+++ README.md
@@ -0,0 +1,13 @@
+Calculator App from Bootcamp Course von Supercode https://www.super-code.de/
+
+Used:
+
+HTMLCollection
+for loop
+addEventListner
+functions
+if statment
+
+Feature:
+
+basic operation calculator
--- script.js
+++ script.js
@@ -1,47 +1,73 @@
-
+// declaring the variables of the HTML elements
 var result = document.querySelector('.result');
 var plus = document.querySelector('.plus');
+var square = document.getElementById("square");
+var dot = document.getElementById("dot");
+var plus = document.getElementById("plus");
+var equal = document.getElementById("equal");
 
 
+// making HTML Collection for the inputs to avoid making event for each one
 var myCollection = document.getElementsByTagName('input');
+console.log(myCollection)
 var txt = " ";
 
+
+// for loop to search through the buttons and find the right one that it clicked
 for(var i = 0; i< myCollection.length ; i++) {
+
+    // adding event for each one
     myCollection[i].addEventListener('click',function(e) {
-        if(e.target.defaultValue=="="){
-            txt=txt;
-            console.log("after = " +txt)
-        } else if(e.target.defaultValue=="C"){
-            txt="";
-            result.innerHTML = txt;
-            console.log("after c " +txt)
-        } else if(e.target.defaultValue=="Del"){
-            txt = txt.substring(0,txt.length-1);
-             result.innerHTML = txt;
-             console.log("after del " +txt)
-        } else if(txt[1] == '+') {
-            txt= " "
-            result.innerHTML = txt;
-            console.log("after + " +txt)
-        }else{
-          txt += e.target.defaultValue;
-          result.innerHTML = txt;  
-          console.log(" " +txt)
+
+        console.log(e)
+
+        
+        // function for showing the elements on the display
+        function showing() {
+            txt += e.target.defaultValue;
+            result.innerHTML = txt;  
         }
+
+
+
+        // making clear when the user press on C
+        if(e.target.value == 'C') {
+            txt = "";
+            result.innerHTML = txt;
+        }
+
+
+
+        // showing the numbers when all the buttons pressed except (=)
+        else if(e.target.value != '=') {
+           
+            // avoiding more than one dot pressing
+            if(result.innerHTML.substr(result.innerHTML.length-1,1) == ".") {
+                if(e.target.value != '.') {
+                    showing();
+                }
+
+            }else {
+                showing();
+            }
+        }
+
+
+
+         // (=) buttons pressing and getting the result from the operations
+        else if(e.target.value == '=') {
+            txt = eval(result.innerHTML);
+            result.innerHTML = txt;
+        }
+
     });
-}
+         
+};
 
 
-document.getElementById("equal").addEventListener("click",()=>{
-    // console.log(eval(document.getElementById("result").innerHTML));
-    // result.innerHTML = eval(document.getElementById("result").innerHTML); 
-    txt = eval(document.getElementById("result").innerHTML);
-    result.innerHTML = txt;
-})
 
-// var str = "helloworld";
-// var last = str.substring(0,1);
-// console.log(last); 
+        
+
 
 
 
